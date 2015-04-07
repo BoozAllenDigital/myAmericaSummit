2624e00d6d0a8e1b6ce65668f820882d6b5ab4e0
samimirimiri <mirimiri_sami@bah.com>
2015-04-07 16:55:12 -0400

    search results formatted

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,34 @@
  * Controller of the myAmericaApp
  */
 angular.module('myAmericaApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $rootScope, $location, RidbSearch, RidbActivities, RIDB_API_KEY) {
+
+    $scope.firstOptions = ['young','young at heart'];
+    $scope.secondOptions = ['weekend-er', 'long-er'];
+    $scope.lat = 37.431573;
+    $scope.lng = -78.656894;
+
+    $scope.firstAnswered = function(answer) {
+      $scope.showSecond = true;
+      $scope.answer1 = answer;
+      var firstAnswer = {"questionId": "1", "selectedOption": answer, "email": $('#email').val()};
+      // totalQuery["answer1"] = answer;
+    };
+
+    $scope.secondAnswered = function(answer) {
+      $scope.showThird = true;
+      $scope.answer2 = answer;
+      var firstAnswer = {"questionId": "1", "selectedOption": answer, "email": $('#email').val()};
+
+      $rootScope.$broadcast('questionsAnswered', { "answer1":$scope.answer1, "answer2":$scope.answer2, "lat":$scope.lat, "lng":$scope.lng });
+      $location.path('/results');
+
+      // totalQuery["answer2"] = answer;
+    };
+
+    RidbActivities.get({"apikey": RIDB_API_KEY}, function(activities) {
+      console.log(activities);
+      $scope.activities = activities;
+    });
+
   });
--- app/scripts/controllers/results.js
+++ app/scripts/controllers/results.js
@@ -8,22 +8,26 @@
  * Controller of the myAmericaApp
  */
 angular.module('myAmericaApp')
-  .controller('ResultsCtrl', function ($scope, RidbSearch, RidbActivities, RIDB_API_KEY) {
+  .controller('ResultsCtrl', function ($scope, RidbSearch, RIDB_API_KEY) {
+
+    $scope.$on('questionsAnswered', function(event, args) {
+      console.log('caught broadcast');
+      $scope.age = args["answer1"];
+      $scope.lengthOfStay = args["answer2"];
+      $scope.lat = args["lat"];
+      $scope.lng = args["lng"];
+      console.log(age + ',' + length);
+      // do what you want to do
+    });
 
     var kids = false;
     var state = 'VA';
     var activities = 14;
 
-    RidbSearch.get({"apikey": RIDB_API_KEY, "state": state, "activity" : activities}, function(results) {
+    RidbSearch.get({"apikey": RIDB_API_KEY, "latitude": $scope.lat, "longitude": $scope.lng, "activity" : activities}, function(results) {
       console.log(results);
       $scope.results = results;
     });
-
-    RidbActivities.get({"apikey": RIDB_API_KEY}, function(activities) {
-      console.log(activities);
-      $scope.activities = activities;
-    });
-
 
 
   });

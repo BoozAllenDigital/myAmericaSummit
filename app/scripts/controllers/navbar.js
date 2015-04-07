190d30e0578df1a24b5f876bc3f5752f7ca06b87
samimirimiri <mirimiri_sami@bah.com>
2015-04-07 13:40:41 -0400

    navbar added

---
+++ app/scripts/controllers/navbar.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myAmericaApp.controller:NavCtrl
+ * @description
+ * # NavCtrl
+ * Controller of the myAmericaApp
+ */
+angular.module('myAmericaApp')
+  .controller('NavCtrl', function ($scope) {
+
+  });
